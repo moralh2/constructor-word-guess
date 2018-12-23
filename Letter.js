2adb6c0610e6b6a847d029a532186a81b16f0d57
Hector M <moralh2@gmail.com>
2018-12-22 21:53:57 -0600

    add reqs

---
+++ Letter.js
@@ -0,0 +1,9 @@
+function Letter() {
+    //display underscore if not to be shown
+    // if you use toString then console log will call when outputting
+
+    // string for character
+    // bool if guessed
+    // func to display '_'
+    // takes char and upds bool
+}
---
+++ Word.js
@@ -0,0 +1,7 @@
+var Letter = require("./Letter")
+
+function Word() {
+    // array of letters
+    // func returns string representing 
+    // fnc takes letter as argument, calls for each letter
+}
