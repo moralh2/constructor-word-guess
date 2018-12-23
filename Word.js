e161a63e13cc1292231813b42cc7f74e86523628
Hector M <moralh2@gmail.com>
2018-12-22 22:15:12 -0600

    testing letter js

--- Word.js
+++ Word.js
@@ -1,6 +1,16 @@
 var Letter = require("./Letter")
 
-function Word() {
+function Word(originalWord) {
+    this.array = []
+    this.create = function(originalWord){
+        var characters = originalWord.split('');
+        for (i = 0; i < characters.length; i++) {
+            var letter = characters[i]
+            this.array.push(new Letter(letter))
+        }
+    this.print = function() {
+        console.log(this.word.join(' '))
+    }
     // array of letters
     // func returns string representing 
     // fnc takes letter as argument, calls for each letter
