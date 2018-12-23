c4bbe630b119eeae75606f03365adb1d2ead3102
Hector M <moralh2@gmail.com>
2018-12-22 23:04:49 -0600

    created inquirer function

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
+var inquirer = require('inquirer')
 var Word = require("./Word")
+var loop = 10
+
+var guessTheWord = function(loop) {
+    if (loop > 0) {
+        inquirer.prompt({
+            name: "guess",
+            message: "What letter do you want to try?"
+        }).then(function(answers){
+            loop--
+            palabra.check(answers.guess)
+            guessTheWord(loop)
+        })
+    }
+}
 
 
 var palabra = new Word('Josefina')
 palabra.createLetters()
-palabra.print()
-palabra.check('a')
-// palabra.print()
-
-
-
-palabra.check('x')
-palabra.check('a')
-palabra.check('j')
+guessTheWord(loop)
